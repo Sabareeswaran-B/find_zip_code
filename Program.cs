7a3babd62afac1d66b2892b6422a6ea79e7e416f
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-01-27 16:57:51 +0530

    added postal api

--- Program.cs
+++ Program.cs
@@ -1,25 +1,30 @@
 ﻿using Newtonsoft.Json.Linq;
 using System.Net.Http;
 
-
 namespace Home
 {
     public class Zipcode
     {
-
         private static void showPostOffice(JObject response)
         {
             JArray? postOffices = (JArray?)response["PostOffice"];
-            Console.WriteLine($"Number of postoffice(s) found in this pincode: {postOffices!.Count}");
+            Console.WriteLine(
+                $"Number of postoffice(s) found in this pincode: {postOffices!.Count}"
+            );
             foreach (var postOffice in postOffices!)
             {
-                string address = $"{postOffice["Name"]}, {postOffice["District"]}, {postOffice["State"]}.";
+                string address =
+                    $"{postOffice["Name"]}, {postOffice["District"]}, {postOffice["State"]}.";
                 Console.WriteLine(address);
             }
         }
-        private static async Task findPlace(int code){
+
+        private static async Task findPlace(int code)
+        {
             HttpClient httpClient = new HttpClient();
-            var responseTask = await httpClient.GetAsync($"https://api.postalpincode.in/pincode/{code}");
+            var responseTask = await httpClient.GetAsync(
+                $"https://api.postalpincode.in/pincode/{code}"
+            );
             var response = await responseTask.Content.ReadAsStringAsync();
             JArray jsonArray = JArray.Parse(response);
             // JObject jsonResponse = JObject.Parse(result);
